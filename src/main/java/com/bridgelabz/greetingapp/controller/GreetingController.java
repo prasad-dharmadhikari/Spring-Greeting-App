87f50d70da62eba63491a0b968dfb8ce7e9b0007
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-01 00:00:02 +0530

    REFACTORED : UC2 Code according to service layer

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,18 +1,28 @@
 package com.bridgelabz.greetingapp.controller;
 
 import com.bridgelabz.greetingapp.model.Greeting;
+import com.bridgelabz.greetingapp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
+    @Autowired
+    GreetingService greetingService;
+
+    @RequestMapping(value = {"/", "", "/home"})
+    public String sayHello() {
+        return greetingService.sayHello();
+    }
+
     @GetMapping("/getGreeting")
     public Greeting getGreeting(@RequestParam(value = "name", defaultValue = "world") String name) {
-        return new Greeting(name);
+        return greetingService.getMessage(name);
     }
 
     @PutMapping("/greetingParam/{name}")
     public Greeting getGreetingByParameter(@PathVariable String name) {
-        return new Greeting(name);
+        return greetingService.getGreetingByParameter(name);
     }
 }
