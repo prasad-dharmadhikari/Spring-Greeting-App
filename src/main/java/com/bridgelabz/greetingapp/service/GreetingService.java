823a122412d3d44a6677951d94091ffa2c905361
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-01 00:01:01 +0530

    ADDED : UC2 Service layer to respond accordingly

--- src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
+++ src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
@@ -1,4 +1,20 @@
 package com.bridgelabz.greetingapp.service;
 
+import com.bridgelabz.greetingapp.model.Greeting;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GreetingService {
+
+    public Greeting getMessage(String name) {
+        return new Greeting(name);
+    }
+
+    public Greeting getGreetingByParameter(String name) {
+        return new Greeting(name);
+    }
+
+    public String sayHello() {
+        return "Hello World";
+    }
 }
