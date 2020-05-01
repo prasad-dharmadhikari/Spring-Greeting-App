84ce3e35b978dbc8cc6738ac28e9f538899c9516
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-02 02:41:52 +0530

    ADDED : Entity class for table in database

--- src/main/java/com/bridgelabz/greetingapp/entity/Greeting.java
+++ src/main/java/com/bridgelabz/greetingapp/entity/Greeting.java
@@ -1,13 +1,51 @@
-package com.bridgelabz.greetingapp.entiry;
+package com.bridgelabz.greetingapp.entity;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.jpa.*;
+import javax.persistence.*;
 
+@Entity
 public class Greeting {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long id;
 
-    int id;
-    String firstName;
-    String lastName;
-    String greetingMessage;
+    @Column
+    public String firstName;
 
+    @Column
+    public String lastName;
+
+    @Column
+    public String greetingMessage;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGreetingMessage() {
+        return greetingMessage;
+    }
+
+    public void setGreetingMessage(String greetingMessage) {
+        this.greetingMessage = greetingMessage;
+    }
 }
