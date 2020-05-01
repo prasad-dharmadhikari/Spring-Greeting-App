aae1ef7dd1e8715481f89ceb69d6d5e0ad8dcf70
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-02 02:42:44 +0530

    ADDED : JPA repository implementor interface

--- src/main/java/com/bridgelabz/greetingapp/repository/GreetingRepository.java
+++ src/main/java/com/bridgelabz/greetingapp/repository/GreetingRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GreetingRepository extends JpaRepository<Greeting, String> {
-    @Override
-    <S extends Greeting> S save(S entity);
+public interface GreetingRepository extends JpaRepository<Greeting, Long> {
+
 }
