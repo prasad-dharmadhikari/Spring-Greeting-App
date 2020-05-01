b411282f7d0a0361c435584e286d3b1562561491
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-02 02:40:11 +0530

    ADDED : GreetingDTO class to return JSON data

--- src/main/java/com/bridgelabz/greetingapp/dto/GreetingDTO.java
+++ src/main/java/com/bridgelabz/greetingapp/dto/GreetingDTO.java
@@ -1,4 +1,41 @@
 package com.bridgelabz.greetingapp.dto;
 
 public class GreetingDTO {
+    Long id;
+    String firstName;
+    String lastName;
+    String greetingMessage;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGreetingMessage() {
+        return greetingMessage;
+    }
+
+    public void setGreetingMessage(String greetingMessage) {
+        this.greetingMessage = greetingMessage;
+    }
 }
