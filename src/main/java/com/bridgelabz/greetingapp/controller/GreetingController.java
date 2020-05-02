42e921c2d7053fae477cce27434fcef2aec1b431
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-02 18:04:45 +0530

    ADDED : Get mapping to get greeting message by Id

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.greetingapp.controller;
 
 import com.bridgelabz.greetingapp.dto.GreetingDTO;
-import com.bridgelabz.greetingapp.entity.Greeting;
+import com.bridgelabz.greetingapp.exception.GreetingAppException;
 import com.bridgelabz.greetingapp.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -26,9 +26,13 @@ public class GreetingController {
     public String getGreetingByParameter(@RequestParam(value = "firstName") String firstName, @RequestParam(value = "lastName") String lastName) {
         return greetingService.getGreetingByParameter(firstName, lastName);
     }
+
     @PostMapping("/create")
     public GreetingDTO saveGreeting(@RequestBody GreetingDTO greetingDTO) {
         return greetingService.saveGreeting(greetingDTO);
     }
-
+    @GetMapping("/greetings/{id}")
+    public GreetingDTO getGreetingById(@PathVariable Long id) throws GreetingAppException {
+        return greetingService.getGreetingByID(id);
+    }
 }
