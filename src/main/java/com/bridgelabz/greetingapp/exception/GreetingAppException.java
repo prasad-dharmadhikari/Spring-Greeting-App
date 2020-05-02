e7e39a8ff578a76c0db121de6d0a41732e023571
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-05-02 18:05:52 +0530

    ADDED : Class to throw exception

--- src/main/java/com/bridgelabz/greetingapp/exception/GreetingAppException.java
+++ src/main/java/com/bridgelabz/greetingapp/exception/GreetingAppException.java
@@ -1,4 +1,13 @@
 package com.bridgelabz.greetingapp.exception;
 
-public class GreetingAppException {
+public class GreetingAppException extends Exception {
+
+    public enum ExceptionType { DATA_NOT_FOUND }
+
+    public ExceptionType type;
+
+    public GreetingAppException(ExceptionType type, String message) {
+        super(message);
+        this.type = type;
+    }
 }
